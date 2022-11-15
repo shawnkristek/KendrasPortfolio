62542f4d83ca20fa3a6e471cef06f195529f7c47
BSwole <shawnkristek@gmail.com>
2022-11-15 07:02:55 -0700

    - installed fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      <h1 className="brown-sugar">
-        Kendra Wright
-      </h1>
-      <p>
-        Hey, I’m Kendra! I’m a UX Designer in Salt Lake City. I’ve always been creative and a problem solver, but also data driven and analytical. I love that UX allows me to put all of these skills and passions to use. I also have a deep drive to help others and spent many years in customer support roles empathizing with my users. I have experience in many different industries, including e-commerce, real estate, health & fitness, food service, and beauty & wellness.
-      </p>
-      <p>
-        Some of the things I love are nature, fantasy books, my cats, cool tattoos, baking, iced coffee, and martial arts.
-      </p>
+    <div className="flex flex-row">
+      <div>Image</div>
+      <div className="flex flex-col">
+        <h1 className="brown-sugar text-9xl">
+          Kendra Wright
+        </h1>
+        <p>
+          Hey, I’m Kendra! I’m a UX Designer in Salt Lake City. I’ve always been creative and a problem solver, but also data driven and analytical. I love that UX allows me to put all of these skills and passions to use. I also have a deep drive to help others and spent many years in customer support roles empathizing with my users. I have experience in many different industries, including e-commerce, real estate, health & fitness, food service, and beauty & wellness.
+        </p>
+        <p>
+          Some of the things I love are nature, fantasy books, my cats, cool tattoos, baking, iced coffee, and martial arts.
+        </p>
+      </div>
     </div>
   );
 }
