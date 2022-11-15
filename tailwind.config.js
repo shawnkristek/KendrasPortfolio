92f230d5ff6971c1e51363f797539b9657f2d0d2
BSwole <shawnkristek@gmail.com>
2022-11-15 06:31:02 -0700

    - installed tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js.jsx,ts.tsx}",
+    "./public/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
