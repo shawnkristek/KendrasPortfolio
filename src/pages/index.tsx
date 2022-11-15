38bd5535eb4c0cf86dfab6ec2680780da1096c9c
BSwole <shawnkristek@gmail.com>
2022-11-15 13:11:14 -0700

    - restructured folders - added basic footer - added navbar placeholder - added main content layout with above - added nextjs boilerplate ideas: src, utils, meta

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import { useRouter } from 'next/router';
+
+import {AppConfig} from '../utils/AppConfig';
+
+import Meta from '../layouts/meta';
+import Main from '../layouts/main';
+import Intro from '../components/intro';
+
+export default function Home() {
+  return (
+    <Main 
+      meta={
+        <Meta
+          title={AppConfig.title}
+          description={AppConfig.description}
+        />
+      }
+    >
+      <Intro />
+    </Main>
+  );
+}
