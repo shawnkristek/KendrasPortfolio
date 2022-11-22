5c9f92e1bdc0628beec42a61089c1ebbf49d39bb
BSwole <shawnkristek@gmail.com>
2022-11-21 19:02:32 -0700

    added all placeholder content to case studies page

--- src/pages/case-studies.js
+++ src/pages/case-studies.js
@@ -1,22 +1,70 @@
 import Meta from "../layouts/meta";
 import Main from "../layouts/main";
 
-export default function CaseStudies() {
-    return (
-        <Main
-            meta={
-                <Meta
-                    title="KW: Case Studies"
-                    description="A collection of Kendra Wrights' favorite UX Design case studies."
-                />
-            }
-        >
-            <div className="grid grid-flow-row gap-[10rem] text-center">
-                <h1 id="" className="gold brown-sugar mt-20">Case Studies</h1>
-                <h1 id="kindle-book-clubs" className="gold brown-sugar mt-20">Kindle Book Clubs</h1>
-                <h1 id="matchbox-marketplace" className="gold brown-sugar mt-20">Matchbox Marketplace</h1>
-                <h1 id="in-progress" className="gold brown-sugar mt-20">In Progress</h1>
-            </div>
-        </Main>
-    );
+import SlideShow from "../components/slideShow";
+import Section from "../components/section";
+import Video from "../components/video";
+import Button from "../components/button";
+
+import { getAllSlides } from "../../lib/slides";
+
+export default function SlideShowPage({
+  kindleSlides,
+  kindleSlidesMobile,
+  matchboxSlides,
+  matchboxSlidesMobile,
+  lejitSlides,
+  lejitSlidesMobile,
+}) {
+  return (
+    <Main
+      meta={
+        <Meta
+          title="Kendra Wright: Case Studies"
+          description="A collection of Kendra Wrights' favorite UX Design case studies."
+        />
+      }
+    >
+        <Section id="kindle-book-clubs">
+          <div className="">
+            <Video />
+            <SlideShow slides={kindleSlides} className="hidden lg:block" />
+            <SlideShow slides={kindleSlidesMobile} className="lg:hidden" mobile={true} />
+          </div>
+        </Section>
+
+        <Section id="matchbox-marketplace" darktheme={false}>
+          <Video />
+          <SlideShow slides={matchboxSlides} className="hidden lg:block" darktheme={false} />
+          <SlideShow slides={matchboxSlidesMobile} className="lg:hidden" darktheme={false} mobile={true} />
+        </Section>
+
+        <Section id="in-progress">
+          <Video />
+          <SlideShow slides={lejitSlides} className="hidden lg:block" />
+          <SlideShow slides={lejitSlidesMobile} className="lg:hidden" mobile={true} />
+          <Button text="but wait, there's more!" />
+        </Section>
+    </Main>
+  );
+}
+
+export async function getStaticProps() {
+  const kindleSlides = getAllSlides("kindleclubs/");
+  const kindleSlidesMobile = getAllSlides("kindleclubs/mobile/");
+  const matchboxSlides = getAllSlides("matchbox/");
+  const matchboxSlidesMobile = getAllSlides("matchbox/mobile/");
+  const lejitSlides = getAllSlides("lejit/");
+  const lejitSlidesMobile = getAllSlides("lejit/mobile/");
+
+  return {
+    props: {
+      kindleSlides,
+      kindleSlidesMobile,
+      matchboxSlides,
+      matchboxSlidesMobile,
+      lejitSlides,
+      lejitSlidesMobile,
+    },
+  };
 }
