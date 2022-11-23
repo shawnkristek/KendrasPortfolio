8d85fa847e16fd2cf4f1af426c4c95e034a7dbc8
BSwole <shawnkristek@gmail.com>
2022-11-22 23:12:51 -0700

    move css modules moved to styles formatted layout for case studies built custom youtube video component built custom slideshow component

--- src/components/video.js
+++ src/components/video.js
@@ -1,10 +1,10 @@
-export default function Video() {
+
+export default function Video({videoUrl, ...props }) {
   return (
-    <div className="flex justify-center align-middle">
+    <div className={`${props.className} flex justify-center align-middle`}>
       <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/ZVuToMilP0A"
+        className="w-full aspect-video"
+        src={videoUrl}
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
--- src/pages/case-studies.js
+++ src/pages/case-studies.js
@@ -1,14 +1,16 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import Meta from "../layouts/meta";
-import Main from "../layouts/main";
+import Meta from "layouts/meta";
+import Main from "layouts/main";
 
-import SlideShow from "../components/slideShow";
-import Section from "../components/section";
-import Video from "../components/video";
-import Button from "../components/button";
+import SlideShow from "components/slideShow";
+import Section from "components/section";
+import Video from "components/video";
+import Button from "components/button";
 
-import { getAllSlides } from "../../lib/slides";
+import { getAllSlides } from "lib/slides";
+
+import styles from "styles/casestudies.module.css";
 
 export default function SlideShowPage({
   kindleSlides,
@@ -18,37 +20,53 @@ export default function SlideShowPage({
   lejitSlides,
   lejitSlidesMobile,
 }) {
+
   return (
     <Main
       meta={
         <Meta
           title="Kendra Wright: Case Studies"
-          description="A collection of Kendra Wrights' favorite UX Design case studies."
+          description="A collection of Kendra Wright's favorite UX Design case studies."
         />
       }
     >
-        <Section id="kindle-book-clubs">
-          <div className="">
-            <Video />
-            <SlideShow slides={kindleSlides} className="hidden lg:block" />
-            <SlideShow slides={kindleSlidesMobile} className="lg:hidden" mobile={true} />
-          </div>
-        </Section>
+      <div className={`${styles.sectionsContainer}`}>
+          <Section id="kindle-book-clubs" className={`gap-20`} >
+            <div className="flex flex-col justify-center align-middle w-1/4">
+              <Video videoUrl="https://www.youtube.com/embed/ZVuToMilP0A" className="" />
+            </div>
+            <div className="flex flex-col justify-center align-middle w-5/12 h-[41.666667%]">
+              <SlideShow slides={kindleSlides} className="" aboveFold={true} scale={0.75} />
+            </div>
+            {/* <SlideShow slides={kindleSlides} className="hidden h-0 lg:flex lg:h-auto" aboveFold={true} scale={0.75} /> */}
+            {/* <SlideShow slides={kindleSlidesMobile} className="lg:hidden" aboveFole={true} scale={1} /> */}
+          </Section>
 
-        <Section id="matchbox-marketplace" darktheme={false}>
-          <Video />
-          <SlideShow slides={matchboxSlides} className="hidden lg:block" darktheme={false} />
-          <SlideShow slides={matchboxSlidesMobile} className="lg:hidden" darktheme={false} mobile={true} />
-        </Section>
+          <Section id="matchbox-marketplace" darktheme={false} className="gap-20">
+            <div className="flex flex-col justify-center align-middle w-1/4">
+              <Video videoUrl="https://www.youtube.com/embed/ZVuToMilP0A" className="" />
+            </div>
+            <div className="flex flex-col justify-center align-middle w-5/12 h-[41.666667%]">
+              <SlideShow slides={matchboxSlides} className="" aboveFold={false} scale={0.75} darktheme={false} />
+            </div>
+            {/* <SlideShow slides={matchboxSlides} darktheme={false} className="hidden lg:flex" scale={0.75} /> */}
+            {/* <SlideShow slides={matchboxSlidesMobile} className="lg:hidden" darktheme={false} mobile={true} /> */}
+          </Section>
 
-        <Section id="in-progress">
-          <Video />
-          <SlideShow slides={lejitSlides} className="hidden lg:block" />
-          <SlideShow slides={lejitSlidesMobile} className="lg:hidden" mobile={true} />
-          <Link href="/designs">
-            <Button text="but wait, there's more!" />
-          </Link>
-        </Section>
+          <Section id="in-progress" className="gap-10">
+            <div className="flex flex-col justify-center align-middle w-1/4">
+              <Video videoUrl="https://www.youtube.com/embed/ZVuToMilP0A" className="" />
+            </div>
+            <div className="flex flex-col justify-center align-middle">
+              <SlideShow slides={lejitSlides} className="" scale={0.75} />
+            </div>
+            {/* <SlideShow slides={lejitSlides} className="hidden lg:flex" scale={0.75} /> */}
+            {/* <SlideShow slides={lejitSlidesMobile} className="lg:hidden" mobile={true} /> */}
+            <Link href="/designs">
+              <Button text="but wait, there's more!" />
+            </Link>
+          </Section>
+      </div>
     </Main>
   );
 }
