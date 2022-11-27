3449926b0b311d638b16335466c2d8502ff2dcad
BSwole <shawnkristek@gmail.com>
2022-11-27 11:00:23 -0700

    changed filetypes to jsx increased gap for intro component in landscape view updated next.js

--- src/layouts/main.jsx
+++ src/layouts/main.jsx
@@ -1,6 +1,7 @@
 import NavMenu from 'components/navmenu';
 import Footer from 'components/footer';
 
+
 export default function Main({ children, meta, ...props }) {
     return (
         <div className="antialiased min-h-screen w-full flex flex-col justify-center lg:text-xl">
